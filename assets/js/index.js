1975380aa88ba9aba429d1a8d0eea616b4699fce
Anthony Thompson <anthonythompson@Anthonys-MacBook-Pro.local>
2020-06-07 12:14:30 -0500

    adding readme and more comments

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+// Import the Crestron Com Library
 import * as CrComLib from "@crestron/ch5-crcomlib";
 
 // Basic Example of a single button
@@ -11,26 +12,25 @@ import * as CrComLib from "@crestron/ch5-crcomlib";
 
 // Example of a container of buttons
 function handleSourcePress(e) {
-
   // Sends value from the button to analog join 1 in simpl
   // have to first convert the value to int
   const value = parseInt(e.target.value, 10);
   CrComLib.publishEvent("n", "1", value);
 
-
   // interlock logic
   // get any source buttons with active css
   var els = document.getElementsByClassName("demoActive");
-  // set the elemeent back to demo
-    Array.from(els).forEach((el) => {
-      document.getElementById(el.id).className = "demo";
-    });
+  // set the elemeent back to inactive
+  Array.from(els).forEach((el) => {
+    document.getElementById(el.id).className = "demo";
+  });
 
-    // now set the source button that was just pressed to active
+  // now set the source button that was just pressed to active
   document.getElementById(`src${e.target.value}`).className = "demoActive";
 }
 
-// listens to the class called sources and runs whenever pressed
+// listens to the class called sources and runs whenever pressed.  So basically any button inside the div
+// will trigger this eventListener to run
 document.querySelector(".sources").addEventListener("click", function (event) {
   if (event.target.tagName === "BUTTON") {
     handleSourcePress(event);
